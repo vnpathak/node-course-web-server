a3fd633a10992f494d70d16bbb9586edf241b7d0
Vishwa Pathak <vnpathak@cygnet.com>
2018-07-18 14:47:14 +0530

    Setup Start Script and Heroku Port

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express=require('express');
 const hbs=require('hbs');
 const fs=require('fs');
+const port=process.env.PORT || 3000;   //process.env staores all environmental variables as key value pair
 
 var app=express();  //initializes express
 
@@ -66,6 +67,6 @@ app.get('/bad',(req,res)=>{
     });
 });
 
-app.listen(3000,()=>{
-    console.log('Server is ready to go on port 3000');
+app.listen(port,()=>{
+    console.log(`Server is ready to go on port ${port}`);
 });
